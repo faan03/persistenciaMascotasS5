7d4a4b714d859cae597b7398b034a39bfe3c70ec
faan03 <faan03@gmail.com>
2017-02-05 18:26:31 -0500

    Initial commit

---
+++ app/src/main/java/com/faan/mascotasrecyclerviewyactionview/Mascota.java
@@ -0,0 +1,41 @@
+package com.faan.mascotasrecyclerviewyactionview;
+
+/**
+ * Created by FAAN on 05/02/2017.
+ */
+
+public class Mascota {
+    private String nombreMascota;
+    private int  raitingMascota;
+    private int  fotoMascota;
+
+    public Mascota (String nombre, int raiting, int foto){
+        this.nombreMascota= nombre;
+        this.raitingMascota=raiting;
+        this.fotoMascota=foto;
+    }
+
+    public String getNombreMascota() {
+        return nombreMascota;
+    }
+
+    public void setNombreMascota(String nombreMascota) {
+        this.nombreMascota = nombreMascota;
+    }
+
+    public int getRaitingMascota() {
+        return raitingMascota;
+    }
+
+    public void setRaitingMascota(int raitingMascota) {
+        this.raitingMascota = raitingMascota;
+    }
+
+    public int getFotoMascota() {
+        return fotoMascota;
+    }
+
+    public void setFotoMascota(int fotoMascota) {
+        this.fotoMascota = fotoMascota;
+    }
+}
